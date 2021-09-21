96193b144d073ddb2d9a8056780847d0e219a9cd
Achiu03 <aion.chiu@isobar.com>
2021-09-22 03:03:42 +0800

    feat: update git ignore.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  coverageDirectory: 'report',
   preset: 'ts-jest',
   testEnvironment: "node",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$"
